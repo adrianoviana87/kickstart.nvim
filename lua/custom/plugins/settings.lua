ad64c7912af31bda34925184a64a705f96154b29
Adriano Viana <adrianoviana87@gmail.com>
2023-03-30 14:33:03 -0300

    add more keymaps

--- lua/custom/plugins/settings.lua
+++ lua/custom/plugins/settings.lua
@@ -2,5 +2,6 @@ vim.wo.relativenumber = true
 
 vim.keymap.set('n', '<leader>ge', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.api.nvim_set_keymap('n', '<leader>f', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', ';', ':', { noremap = true })
 
 return {}
